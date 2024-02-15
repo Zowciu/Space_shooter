4f97defbfa9d1a71fd071991bebacf7e0c5b0b58
Zowciu <szymon.zowczak@gmail.com>
2024-02-15 18:06:15 +0100

    Move function and velocity added

--- include/Enemy.hpp
+++ include/Enemy.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 class Enemy : public sf::Drawable 
@@ -7,11 +8,13 @@ class Enemy : public sf::Drawable
 
 private:
     sf::Sprite enemy;
+    int velocityY;
 
 public:
     Enemy(const sf::Texture &texture, sf::Vector2f position);
     ~Enemy();
 
+    void Move(sf::Vector2f direction);
     sf::Vector2f GetPosition();
     sf::FloatRect GetGlobalBounds();
     void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,16 +1,20 @@
 #include "Enemy.hpp"
 
-Enemy::Enemy(const sf::Texture &texture, sf::Vector2f position) {
+Enemy::Enemy(const sf::Texture &texture, sf::Vector2f position) : velocityY(3) {
 
     enemy.setTexture(texture);
     enemy.setPosition(position);
-    enemy.setScale({0.5f, 0.5f});
+    enemy.setScale({0.1f, 0.1f});
 }
 
 Enemy::~Enemy() {
 
 }
 
+void Enemy::Move(sf::Vector2f direction)
+{
+    enemy.move(direction.x, direction.y * velocityY);
+}
 
 sf::Vector2f Enemy::GetPosition()
 {
@@ -22,5 +26,5 @@ sf::FloatRect Enemy::GetGlobalBounds()
 }
 void Enemy::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
-
+    target.draw(enemy);
 }
